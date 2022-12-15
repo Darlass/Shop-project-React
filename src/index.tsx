6be3ee825efcba326a80e5599577cadcb047bc9d
Daria <mar.031281@gmail.com>
2022-12-15 21:55:51 +0300

    component(function)

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// JSX
-const h1 = <h1 id="biba" className="habiba">Hello World</h1>
-
-// without JSX
-// const h1 = React.createElement(
-//     'h1',
-//     { id: 'title', class: 'biba' },
-//     'Hello React'
-// ) // ('элемент', атрибуты элемента,'контнет')
+//Компонент - функция
+function FirstApp() {
+    return (
+        <div>
+            <h1>Hello react!</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+                dolore perspiciatis aperiam, ex modi quis dignissimos quod. Eos non,
+                praesentium officiis esse, ipsum rerum earum nesciunt libero laborum
+                mollitia labore
+            </p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode><App /></React.StrictMode >)
+root.render(<React.StrictMode><FirstApp /></React.StrictMode >)
