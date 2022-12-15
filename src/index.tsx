08142e02dd86bd0ea2231f9b61c36f5cedf26161
Daria <mar.031281@gmail.com>
2022-12-15 21:54:30 +0300

    how to use JSX ans without JSX

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// JSX
+const h1 = <h1 id="biba" className="habiba">Hello World</h1>
+
+// without JSX
+// const h1 = React.createElement(
+//     'h1',
+//     { id: 'title', class: 'biba' },
+//     'Hello React'
+// ) // ('элемент', атрибуты элемента,'контнет')
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode><App /></React.StrictMode >)
