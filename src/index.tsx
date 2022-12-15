8d4de1e954191fc02b2da2949e51b973807a67e6
Daria <mar.031281@gmail.com>
2022-12-15 21:58:04 +0300

    arrow function, React.Fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//Компонент - функция
-function FirstApp() {
+//Arrow foo
+const Title = () => <h1>Hello arrow foo component</h1>
+const Content = () => {
     return (
-        <div>
-            <h1>Hello react!</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-                dolore perspiciatis aperiam, ex modi quis dignissimos quod. Eos non,
-                praesentium officiis esse, ipsum rerum earum nesciunt libero laborum
-                mollitia labore
-            </p>
-        </div>
+        <React.Fragment>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, optio, laborum similique quo consequatur a incidunt maxime sapiente esse tempora atque, quod molestias eveniet omnis magnam odit nostrum nisi reiciendis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsam explicabo commodi voluptas sapiente placeat quidem necessitatibus a ab! Iusto ullam sapiente hic officia vero dolores laboriosam asperiores officiis maxime.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea error modi cumque delectus quas laboriosam incidunt nesciunt doloremque odio excepturi dolores aut, magni, quos amet esse eius maiores nisi molestiae.</p>
+        </React.Fragment>
     )
 }
 
+const App = () => {
+    return (
+        <React.Fragment>
+            <Title />
+            <Content />
+        </React.Fragment>
+    )
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode><FirstApp /></React.StrictMode >)
+root.render(<React.StrictMode><App /></React.StrictMode >)
