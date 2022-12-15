17fac4178fe00b54da69ef6b49e3ef2bc3407d47
Daria <mar.031281@gmail.com>
2022-12-15 22:09:40 +0300

    short-syntaxis React.Fragment

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,20 @@ import ReactDOM from 'react-dom/client'
 const Title = () => <h1>Hello arrow foo component</h1>
 const Content = () => {
     return (
-        <React.Fragment>
+        <>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, optio, laborum similique quo consequatur a incidunt maxime sapiente esse tempora atque, quod molestias eveniet omnis magnam odit nostrum nisi reiciendis!</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsam explicabo commodi voluptas sapiente placeat quidem necessitatibus a ab! Iusto ullam sapiente hic officia vero dolores laboriosam asperiores officiis maxime.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea error modi cumque delectus quas laboriosam incidunt nesciunt doloremque odio excepturi dolores aut, magni, quos amet esse eius maiores nisi molestiae.</p>
-        </React.Fragment>
+        </>
     )
 }
 
 const App = () => {
     return (
-        <React.Fragment>
+        <>
             <Title />
             <Content />
-        </React.Fragment>
+        </>
     )
 }
 
