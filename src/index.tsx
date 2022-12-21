61c23449fa29b010d83c431eba53d998cfe1e81b
Daria <mar.031281@gmail.com>
2022-12-21 14:50:51 +0300

    some features of props, add new file for component

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// деструктуризация обьекта
-const user = {
-    age: 20,
-    name: "Valeria",
-    property: "nothing"
+
+
+type TitleProps = { // название пропсов пишем с большой буквы(PascalCase)
+    subtitle: string | number // 2) Описали тип данныхs
+    text?: string // Знак ? означает что этот пропр необязательный
 }
-const { age, name, property } = user
-// деструктуризация массива
-const array = [1, 2, 3, 4, 5]
-const [test1, test2, ...test3] = array
 
+const Title = ({ subtitle, text = "Cry" }: TitleProps) => { // 1) Описали данные, если не задают text то можно ставить ее по умолчанию
+    return (<h1>{text} {subtitle}</h1>) // деструктуризация пропса
+}
 
+const Content = () => {
+    return (
+        <>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, optio, laborum similique quo consequatur a incidunt maxime sapiente esse tempora atque, quod molestias eveniet omnis magnam odit nostrum nisi reiciendis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsam explicabo commodi voluptas sapiente placeat quidem necessitatibus a ab! Iusto ullam sapiente hic officia vero dolores laboriosam asperiores officiis maxime.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea error modi cumque delectus quas laboriosam incidunt nesciunt doloremque odio excepturi dolores aut, magni, quos amet esse eius maiores nisi molestiae.</p>
+        </>
+    )
+}
+
+const App = () => {
+    return ( // 3) Вывели данные
+        <>
+            <Title subtitle="Anime" />
+            <Title text="Hello" subtitle="React" />
+            <Content />
+        </>
+    )
+}
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode >)
+root.render(<React.StrictMode><App /></React.StrictMode >)
