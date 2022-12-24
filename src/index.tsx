06ea5fc359a7cd0ac948adfd7d75e4fd31b5766e
Daria <mar.031281@gmail.com>
2022-12-24 12:08:37 +0300

    delete test files

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Content from './Content' // 2.Обязательно импортируем
-
-
-type TitleProps = { // название пропсов пишем с большой буквы(PascalCase)
-    subtitle: string | number // 2) Описали тип данныхs
-    text?: string // Знак ? означает что этот пропр необязательный
-}
-
-const Title = ({ subtitle, text = "Hello" }: TitleProps) => { // 1) Описали данные, если не задают text то можно ставить ее по умолчанию
-    return (<h1>{text} {subtitle}</h1>) // деструктуризация пропса
-}
 
 const App = () => {
-    return ( // 3) Вывели данные 3. Выводим компонент на страницу
-        <>
-            <Title subtitle="my stugglings" />
-            <Content p1="My love always for" p2="You" />
-        </>
-    )
+    return <></>
 }
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<React.StrictMode><App /></React.StrictMode >)
