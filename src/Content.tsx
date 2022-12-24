eac3e8f5ac1594b85f4fdbacd96e8a2207882f01
Daria <mar.031281@gmail.com>
2022-12-24 12:01:28 +0300

    css modules how to write classes, why we used []

--- src/Content.tsx
+++ src/Content.tsx
@@ -7,7 +7,7 @@ type ContentProps = {
 const Content = ({ p1, p2 }: ContentProps) => {
     return (
         <>
-            <p className={`${classes.love} ${classes['font']}`}>{p1}</p>
+            <p className={`${classes.love} ${classes.font}`}>{p1}</p>
             <p>{p2}</p>
         </>
     )
