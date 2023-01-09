dc25076aa78ac88df63da274232bc816695addef
Daria <mar.031281@gmail.com>
2023-01-09 19:36:39 +0300

    add menu

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,11 +1,11 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import './Header.scss'
+import Menu from 'components/Menu/Menu';
 
 type Props = {}
 const Header = (props: Props) => {
@@ -25,10 +25,10 @@ const Header = (props: Props) => {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         News
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Menu />
                 </Toolbar>
             </Container>
-        </AppBar>
+        </AppBar >
     )
 }
 export default Header 
