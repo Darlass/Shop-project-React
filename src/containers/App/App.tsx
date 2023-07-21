2ab998a8cfae818d05d824ec9dd1ca4db5a789de
Daria <mar.031281@gmail.com>
2023-07-21 11:07:03 +0300

    add some notes

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -5,16 +5,31 @@ import Main from 'containers/Main/Main'
 import { useState } from 'react'
 
 type CartDataProps = {
+    // удалить
     totalCount: number
     totalPrice: number
 }
+
+type ProdcuctsInCartProps = {
+    [id: number]: number // Айди записываем в квадртаных скобках, потому что это динамические даные.
+}
+
 const App = () => {
     const [cartData, setCartData] = useState<CartDataProps>({
+        // удалить
         totalCount: 0,
         totalPrice: 0,
     })
+ // Товары в корзину нужно добавлять по количеству и значению 
+    const [productsInCart, setProductsInCart] = useState<ProdcuctsInCartProps>({
+        // этот обьект динамичный
+        1: 5, // айди = ключ, ко-ство = значение
+        2: 5,
+    })
+
     const addToCart = (count: number, price: number) => {
         setCartData((prevState: CartDataProps) => ({
+            // удалить, добавить новую функцию которая будет добавлять данные в обьект.
             totalCount: prevState.totalCount + count,
             totalPrice: prevState.totalPrice + count * price,
         }))
